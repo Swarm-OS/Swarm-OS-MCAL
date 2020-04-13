ebb44b4f1eaa5db5a114d47219ee5be35deeb137
ChristophLehr <christoph.lehr@gmx.at>
2020-04-13 15:52:59 +0200

    added documentation

--- includes/datatypes.h
+++ includes/datatypes.h
@@ -11,11 +11,11 @@ typedef enum
 
 typedef enum
 {
-    E_OK = 0x00,
-    E_NOT_SUPPORTED = 0x01,
-    E_NOT_IMPLEMENTED = 0x02,
-    E_VALUE_ERR = 0x03,
-    E_ERR = 0xFF,
+    E_OK                = 0x00,     //  Everything ok, no error
+    E_NOT_SUPPORTED     = 0x01,     //  This is not supported on the MCU
+    E_NOT_IMPLEMENTED   = 0x02,     //  Function not implemented yet
+    E_VALUE_ERR         = 0x03,     //  Input value error
+    E_ERR               = 0xFF,     //  General error
 } std_return_type_t;
 
 #endif
